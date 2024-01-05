0462b3976e6ab14c99342ffacab505da6a3598da
Dialdent <147373144+Dialdent@users.noreply.github.com>
2024-01-05 13:42:53 +0200

    Update custom-min.js

--- assets/js/custom-min.js
+++ assets/js/custom-min.js
@@ -1,3 +1,4 @@
 function clickButton(c){document.getElementById(c).click()};
 
 function navPage(c){window.location.href='#'+c;history.pushState({},'','/'+c)}
+if (performance.navigation.type === 1) {window.location.replace(window.location.origin);}
