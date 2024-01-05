dd0c1360020a197ad96bbde6c6cb2921f63508fc
Dialdent <147373144+Dialdent@users.noreply.github.com>
2024-01-05 13:58:19 +0200

    Replacing href with onClick

--- assets/js/custom-min.js
+++ assets/js/custom-min.js
@@ -1,4 +1,3 @@
-function clickButton(c){document.getElementById(c).click()};
+function clickButton(c){document.getElementById(c).click();navPage(c)};
 
-function navPage(c){window.location.href='#'+c;history.pushState({},'','/'+c)}
-if (performance.navigation.type === 1) {window.location.replace(window.location.origin);}
+function navPage(c){window.location.href='#'+c;history.pushState({},'','/')}
